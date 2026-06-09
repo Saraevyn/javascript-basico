59dbb88e0aba9696590ef2c66691cfae229e9b2f
Sara Evellin Duarte <saraevyn@outlook.com>
2026-06-09 16:32:25 -0300

    Update script.js

--- exercicios-resolvidos/js/script.js
+++ exercicios-resolvidos/js/script.js
@@ -10,3 +10,17 @@ if (numero1 > numero2) {
 } else {
     console.log("Os dois números são iguais!");
 }
+
+// 2//
+
+let valor = parseFloat(prompt("Digite um número: "));
+
+if (valor > 0) {
+    console.log("O valor é POSITIVO.");
+} else if (valor < 0) {
+    console.log("O valor é NEGATIVO.");
+} else {
+    console.log("O valor é ZERO (neutro).");
+}
+
+
